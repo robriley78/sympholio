f62c51a28e798978dc843df811b441f8dc62c1b1
robriley78 <rob.riley@me.com>
2012-12-23 21:54:57 +0000

    Added new regions, crude CSS to make regions obvious.

--- page.tpl.php
+++ page.tpl.php
@@ -121,31 +121,35 @@
     <?php print $messages; ?>
 
     <div id="main-wrapper"><div id="main" class="clearfix">
+    
+		<div id="navigation"><div class="section">
+		<?php print render($page['navigation']); ?>
+		</div></div> <!-- /.section, /#navigation -->
+    
+		<?php if ($page['sidebar_first']): ?>
+		<div id="sidebar-first" class="column sidebar"><div class="section">
+		<?php print render($page['sidebar_first']); ?>
+		</div></div> <!-- /.section, /#sidebar-first -->
+		<?php endif; ?>
 
-      <div id="content" class="column"><div class="section">
-        <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-        <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-        <?php print render($title_suffix); ?>
-        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
-        <?php print render($page['help']); ?>
-        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-        <?php print render($page['content']); ?>
-        <?php print $feed_icons; ?>
-      </div></div> <!-- /.section, /#content -->
+		<div id="content" class="column"><div class="section">
+		<?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+		<a id="main-content"></a>
+		<?php print render($title_prefix); ?>
+		<?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+		<?php print render($title_suffix); ?>
+		<?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+		<?php print render($page['help']); ?>
+		<?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+		<?php print render($page['content']); ?>
+		<?php print $feed_icons; ?>
+		</div></div> <!-- /.section, /#content -->
 
-      <?php if ($page['sidebar_first']): ?>
-        <div id="sidebar-first" class="column sidebar"><div class="section">
-          <?php print render($page['sidebar_first']); ?>
-        </div></div> <!-- /.section, /#sidebar-first -->
-      <?php endif; ?>
-
-      <?php if ($page['sidebar_second']): ?>
-        <div id="sidebar-second" class="column sidebar"><div class="section">
-          <?php print render($page['sidebar_second']); ?>
-        </div></div> <!-- /.section, /#sidebar-second -->
-      <?php endif; ?>
+		<?php if ($page['sidebar_second']): ?>
+		<div id="sidebar-second" class="column sidebar"><div class="section">
+		<?php print render($page['sidebar_second']); ?>
+		</div></div> <!-- /.section, /#sidebar-second -->
+		<?php endif; ?>
 
     </div></div> <!-- /#main, /#main-wrapper -->
 
